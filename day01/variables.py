d5438878c5f303c2e191a04fd54353de10775da8
efecagrisen <efecagrisen@yahoo.com>
2025-05-27 12:30:08 +0300

    operators

--- day01/variables.py
+++ day01/variables.py
@@ -34,4 +34,31 @@ print("I am {} years old".format(age))
 print(f"I am {age} years old")
 #f stands for format function
 
+print("--Operators---------------------------------")
+
+str1 = "python programming"
+
+print("python" in str1)
+
+print(True and True)  #&&
+print(False or True)  #||
+
+s1 = "Java"
+s2 = "Java"
+
+print(s1 is s2) #used for checking if two reference variables are referencing to the same objects (== in java)
+
+"""
+in Java;
+
+String s1 = "Java";
+String s2 = "Java";
+
+String s3 = new String("Java");
+
+s1 == s2 ---> true
+s1 == s3 ---> false
+"""
+
+
 
