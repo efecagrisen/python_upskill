338d19f9a2b6edeff9569b04bdf637523b11da6f
efecagrisen <efecagrisen@yahoo.com>
2025-05-27 11:51:01 +0300

    initial commit

---
+++ day01/first_python_program.py
@@ -0,0 +1,14 @@
+print("Hello world!")
+
+#this is a comment
+
+"""
+this is 
+a 
+multiline
+comment
+"""
+
+print("I love \"Python!\"")
+print('I love "Python!"')
+#these 2 are the same
---
+++ day01/variables.py
@@ -0,0 +1,37 @@
+"""
+    Java variables:
+    String varName = data;
+
+    Python variables:
+    varName = data;
+"""
+
+name_of_student = "Efe"
+print(type(name_of_student))
+print(name_of_student)
+
+name_of_student = 3.5
+print(type(name_of_student))
+print(name_of_student)
+
+name_of_student = True
+print(type(name_of_student))
+print(name_of_student)
+
+
+print("---")
+
+s = "35"
+
+num = int(s)
+
+print(num)
+print(type(num))
+
+#concatenation
+age = 20
+print("I am {} years old".format(age))
+print(f"I am {age} years old")
+#f stands for format function
+
+
